8c8f383eb701c5d1133d6c431319012eef5b8cee
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2025-06-24 02:07:14 +0300

    added testmoniar reciters

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const qouteArea = document.querySelector(".graph");
+const reciterArea = document.querySelector(".reciter");
 
 let imagesCenter = ["images/buggatti.jpeg","images/camero.jpeg","images/lambo.jpeg"];
 
 let statements = ["Bugatti is a French luxury hyper sports car manufacturer, renowned for its high-performance vehicles. Founded in 1909 by Ettore Bugatti",
                     "The Chevrolet Camaro is a popular American pony car known for its performance and classic design. It was first introduced in 1967 to compete with the Ford Mustang,",
                     "Lamborghini, an Italian brand known for its high-performance sports cars, was founded in 1963 by Ferruccio Lamborghini."];
+let reciters = ["Teefe K","kadaada J","isma S"]
 
 function qouteDislay(count){
     let qouteCounter = count;
     console.log(qouteArea.textContent);
     qouteArea.textContent = statements[qouteCounter];
+    reciterArea.textContent = reciters[qouteCounter];
     if (qouteCounter < 3){
         qouteCounter++;
         setTimeout(function(){
